ac43da1da6a476a95ed1d3fa6ca5ff471274074d
itse <ilsetse@gmail.com>
2017-02-06 22:26:40 -0800

    bootstrap base

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,19 @@
-function openNav() {
-    var x = document.getElementById("menu");
-    if (x.className === "navbar") {
-        x.className += " responsive";
+/*
+$(window).scroll(function() { // on scroll, get the browser window and trigger function
+    if ( $(this).scrollTop() > 600) {
+        $(".navbar-fixed-top").css('margin-top','0');
+        
     } else {
-        x.className = "navbar";
-    }
+        $(".navbar-fixed-top").css('margin-top',600-$(this).scrollTop());
+        //$(".navbar-fixed-top").removeAttr('style'); // what is this sorcery
+        }
+});
+
+
+$(document).ready(function(){
+    $(".landing-page").css('min-height',$(window).height());
+    $("#landing-page-cover").css('min-height',$(window).height());
 }
+*/
+
+$('.carousel').carousel()
