fb75fa261d4d5adae1b41740a07c4d22fb34777f
ilsetse <ilsetse@gmail.com>
2017-10-12 19:07:59 +0300

    v2 new ep

---
+++ custom.js
@@ -0,0 +1,11 @@
+
+// external js:
+// masonry.pkgd.js
+
+
+$('.grid').masonry({
+									 itemSelector: '.grid-item',
+									 columnWidth: 160, // width of the grid in px
+									 
+									 isFitWidth: true // fill page width
+									 });
